545ef93c45a7996bb0e3babeeb1ee3f045468723
Midhun D <midhun.9.5508@gmail.com>
2026-07-09 19:06:10 +0530

    configured eureka discovery server and registered microservices

--- Week_4/Microservices_SpringBoot3_SpringCloud/loan/loan/src/main/java/loan/LoanApplication.java
+++ Week_4/Microservices_SpringBoot3_SpringCloud/loan/loan/src/main/java/loan/LoanApplication.java
@@ -2,12 +2,13 @@ package loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LoanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoanApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplication.class, args);
+    }
 }
